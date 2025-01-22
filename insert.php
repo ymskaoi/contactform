23ea41e552e75f051d81b17366e5c0382e41585b
ymskaoi <ymsk_aoi@icloud.com>
2025-01-22 21:45:14 +0900

    Lesson_contactform

---
+++ insert.php
@@ -0,0 +1,35 @@
+<?php 
+
+mb_internal_encoding("utf8");
+$pdo=new PDO("mysql:dbname=lesson1;host=localhost;","root","root");
+
+$sql="insert into contactform(name,mail,age,comments) value(?,?,?,?)";
+$stmt=$pdo->prepare($sql);
+
+$stmt->bindValue(1, $_POST['name']);
+$stmt->bindValue(2, $_POST['mail']);
+$stmt->bindValue(3, $_POST['age']);
+$stmt->bindValue(4, $_POST['comments']);
+
+$stmt->execute();
+?>
+
+<!DOCTYPE HTML>
+<html lang="ja">
+
+<head>
+    <meta charset="utf-8">
+    <title>お問合わせフォームを作る</title>
+    <link rel="stylesheet"type="text/css" href="style2.css">
+</head>
+
+<body>
+    <h1>お問合わせフォーム</h1>
+
+    <div class="confirm">
+        <p>
+            お問合わせ有難うございました。<br>３営業日以内に担当者よりご連絡差し上げます。
+        </p>
+    </div>
+</body>
+</html>
